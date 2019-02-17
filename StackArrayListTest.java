8c59b21963c6ed9c8f892df8528eed8963d0a32c
Estuardo Ureta <32362738+estuardo8u14@users.noreply.github.com>
2019-02-16 23:25:47 -0600

    Create StackArrayListTest.java

---
+++ StackArrayListTest.java
@@ -0,0 +1,21 @@
+pakagee hdt4;
+public class StackArrayListTest {
+    
+    public StackArrayListTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
